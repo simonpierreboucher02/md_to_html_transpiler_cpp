b151c2caea7f0af4120490432fa228dc151513e1
Simon-Pierrre Boucher <spbou4@protonmail.com>
2025-07-15 21:21:13 -0400

    Add files via upload

---
+++ build/CMakeFiles/markdown-transpiler.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/simon-pierreboucher/Desktop/Markdown_to_HTML_Transpiler /src/generator.cpp" "CMakeFiles/markdown-transpiler.dir/src/generator.cpp.o" "gcc" "CMakeFiles/markdown-transpiler.dir/src/generator.cpp.o.d"
+  "/Users/simon-pierreboucher/Desktop/Markdown_to_HTML_Transpiler /src/lexer.cpp" "CMakeFiles/markdown-transpiler.dir/src/lexer.cpp.o" "gcc" "CMakeFiles/markdown-transpiler.dir/src/lexer.cpp.o.d"
+  "/Users/simon-pierreboucher/Desktop/Markdown_to_HTML_Transpiler /src/main.cpp" "CMakeFiles/markdown-transpiler.dir/src/main.cpp.o" "gcc" "CMakeFiles/markdown-transpiler.dir/src/main.cpp.o.d"
+  "/Users/simon-pierreboucher/Desktop/Markdown_to_HTML_Transpiler /src/parser.cpp" "CMakeFiles/markdown-transpiler.dir/src/parser.cpp.o" "gcc" "CMakeFiles/markdown-transpiler.dir/src/parser.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/markdown-transpiler.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/markdown-transpiler.dir/src/generator.cpp.o"
+  "CMakeFiles/markdown-transpiler.dir/src/generator.cpp.o.d"
+  "CMakeFiles/markdown-transpiler.dir/src/lexer.cpp.o"
+  "CMakeFiles/markdown-transpiler.dir/src/lexer.cpp.o.d"
+  "CMakeFiles/markdown-transpiler.dir/src/main.cpp.o"
+  "CMakeFiles/markdown-transpiler.dir/src/main.cpp.o.d"
+  "CMakeFiles/markdown-transpiler.dir/src/parser.cpp.o"
+  "CMakeFiles/markdown-transpiler.dir/src/parser.cpp.o.d"
+  "bin/markdown-transpiler"
+  "bin/markdown-transpiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/markdown-transpiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
